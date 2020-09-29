a72ffec663e57622eeb1e90a1894f4af0046f2fb
clarence <2263194561@qq.com>
2020-09-29 15:20:27 +0800

    sqlalchemy组件初始化, 延后初始化

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding:utf8 -*-
 import sys
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from os.path import dirname, abspath
 
 # 将common路径添加到模块查询路径中
@@ -10,6 +11,17 @@ sys.path.insert(0, BASE_DIR + '/common')
 
 from app.settings.config import config_dict
 from utils.constants import EXTRA_ENV_COINFIG
+
+
+# sqlalchemy组件对象
+db = SQLAlchemy()
+
+
+def register_extensions(app):
+    """组件初始化"""
+
+    # 延后关联sqlalchemy
+    db.init_app(app)
 
 
 def create_flask_app(type):
@@ -35,6 +47,9 @@ def create_app(type):
     """创建flask应用 和 初始化组件"""
 
     app = create_flask_app(type)
+
+    # 组件初始化
+    register_extensions(app)
 
     return app
 
--- app/settings/config.py
+++ app/settings/config.py
@@ -4,7 +4,14 @@
 
 class DefaultConfig(object):
     """默认配置"""
-    pass
+    # mysql配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:x1430371727@127.0.0.1:3306/top_news'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 是否追踪数据变化
+    SQLALCHEMY_ECHO = True  # 是否打印底层执行的SQL
+
+    # redis配置
+    REDIS_HOST = '127.0.0.1'  # ip
+    REDIS_PORT = 6379  # 端口
 
 
 # 定义字典记录 配置类型和配置子类的关系
