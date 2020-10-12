e488dd171fa893983d55dd91afc3792dba502a33
clarence <2263194561@qq.com>
2020-10-12 17:14:53 +0800

    jwt utils工具封装fix

--- common/utils/jwt_util.py
+++ common/utils/jwt_util.py
@@ -17,9 +17,10 @@ def generate_jwt(payload, expiry, secret=None):
 
     if not secret:
         secret = current_app.config['JWT_SECRET']
-        token = jwt.encode(_payload, secret, algorithm='HS256')
 
-        return token.decode()
+    token = jwt.encode(_payload, secret, algorithm='HS256')
+
+    return token.decode()
 
 
 def verify_jwt(token, secret=None):
