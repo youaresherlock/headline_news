d88fb773e1e214b2cfb19e33435b01e5032bb0e2
clarence <2263194561@qq.com>
2020-10-16 22:00:46 +0800

    文章模型类

--- common/models/article.py
+++ common/models/article.py
@@ -1,6 +1,7 @@
 #!usr/bin/python
 # -*- coding:utf8 -*-
 from app import db
+from datetime import datetime
 
 
 class Channel(db.Model):
@@ -28,6 +29,27 @@ class UserChannel(db.Model):
     sequence = db.Column(db.Integer, default=0, doc='序号')
     is_deleted = db.Column(db.Boolean, default=False, doc='是否删除')
 
+
+class Article(db.Model):
+    """文章基本信息表"""
+    __tablename__ = 'news_article_basic'
+
+    class STATUS:
+        DRAFT = 0  # 草稿
+        UNREVIEWED = 1  # 待审核
+        APPROVED = 2  # 审核通过
+        FAILED = 3  # 审核失败
+        DELETED = 4  # 已删除
+        BANNED = 5  # 封禁
+
+    id = db.Column(db.Integer, primary_key=True, doc='文章id')
+    user_id = db.Column(db.Integer, doc='用户id')
+    channel_id = db.Column(db.Integer, doc='频道id')
+    title = db.Column(db.String(130), doc='标题')
+    cover = db.Column(db.JSON, doc='封面')
+    ctime = db.Column(db.Datetime, default=datetime.now, doc='创建时间')
+    status = db.Column(db.Integer, default=0, doc='帖文状态')
+    comment_count = db.Column(db.Integer, default=0, doc='评论数')
 
 
 
