cd8a21b824d42eb397a4d34ac549946b7c1f6980
clarence <2263194561@qq.com>
2020-10-16 21:02:50 +0800

    修改用户频道类视图

--- app/resources/user/channel.py
+++ app/resources/user/channel.py
@@ -1,13 +1,17 @@
 #!usr/bin/python
 # -*- coding:utf8 -*-
+from app import db
 from flask import g
+from flask import request
 from flask_restful import Resource
 from models.article import Channel
 from sqlalchemy.orm import load_only
 from models.article import UserChannel
+from utils.decorators import login_required
 
 
 class UserChannelResource(Resource):
+    method_decorators = {'put': [login_required]}
 
     def get(self):
         # 获取参数
@@ -33,6 +37,32 @@ class UserChannelResource(Resource):
         # 返回数据
 
         return {'channels': channel_list}
+
+    def put(self):
+        """修改用户频道 重置式更新"""
+        # 获取参数
+        userid = g.userid
+        channels = request.json.get('channels')
+        # 将该用户原有的频道列表逻辑删除
+        UserChannel.query.filter(UserChannel.user_id == userid, UserChannel.is_deleted is False).\
+            update({'is_deleted': True})
+        # 遍历新的频道列表
+        for channel in channels:
+            # 查询是否关注过该频道
+            user_channel = UserChannel.query.options(load_only(UserChannel.id)).filter(
+                UserChannel.user_id == userid, UserChannel.channel_id == channel['id']
+            ).first()
+            if user_channel:  # 如果有, 更新数据(序号和逻辑删除去掉)
+                user_channel.sequence = channel['seq']
+                user_channel.is_deleted = False
+            else:
+                # 如果没有, 新增数据
+                user_channel = UserChannel(user_id=userid, channel_id=channel['id'], sequence=channel['seq'])
+                db.session.add(user_channel)
+        # 将数据返回
+        db.session.commit()
+
+        return {'channels': channels}
 
 
 
