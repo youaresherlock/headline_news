d151336658ce225db80f0f8311ba3298bcf25cf5
clarence <2263194561@qq.com>
2020-10-18 18:58:16 +0800

    文章详情关系数据查询类视图实现

--- app/resources/article/articles.py
+++ app/resources/article/articles.py
@@ -1,10 +1,15 @@
 #!usr/bin/python
 # -*- coding:utf8 -*-
 from app import db
+from flask import g
 from models.user import User
 from datetime import datetime
+from models.user import Relation
 from models.article import Article
 from flask_restful import Resource
+from models.article import Attitude
+from sqlalchemy.orm import load_only
+from models.article import Collection
 from models.article import ArticleContent
 from utils.constants import HOME_PRE_PAGE
 from flask_restful.reqparse import RequestParser
@@ -82,6 +87,34 @@ class ArticleDetailResource(Resource):
             'attitude': -1,
             'is_collected': False
         }
+
+        """查询关系数据"""
+        userid = g.userid
+        if userid:
+            # 查询收藏关系 用户 -> 文章
+            collect = Collection.query.options(load_only(Collection.id)).\
+                filter(Collection.user_id == userid, Collection.article_id == article_id,
+                       Collection.is_deleted is False).first()
+
+            article_dict['is_collected'] = True if collect else False
+
+            # 查询用户关系 用户 -> 作者
+            relation = Relation.query.options(load_only(Relation.id)).\
+                filter(Relation.user_id == userid, Relation.author_id == data.user_id,
+                       Relation.relation == Relation.RELATION.FOLLOW).first()
+
+            article_dict['is_followed'] = True if relation else False
+
+            # 查询用户对文章的态度 用户对文章
+            atti_obj = Attitude.query.options(load_only(Attitude.attitude)).\
+                filter(Attitude.user_id == userid, Attitude.article_id == article_id).first()
+
+            if not atti_obj:  # 没有记录, 则表示没有态度
+                attitude = -1
+            else:
+                attitude = atti_obj.attitude
+
+            article_dict['attitude'] = attitude 
 
         # 返回数据
         return {'data': article_dict}
